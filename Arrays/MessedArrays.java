4f715e507c38df7eeedbcac0d0e4b64589857afb
p@ren <ntimprosper308@gmail.com>
2024-11-03 14:37:53 +0000

    Modified Arrays/ MessedArrays.java

--- Arrays/MessedArrays.java
+++ Arrays/MessedArrays.java
@@ -5,6 +5,16 @@ public class MessedArrays{
             System.out.println("Hello there " + string + ", nice to meet you");
         }
     }
+    public int[] twoSum(int[] nums, int target) {
+	for (int i = 0; i < nums.length; i++) {
+           for (int j = i + 1; j < nums.length; j++) {
+		if (nums[j] == target - nums[i]) {
+		  return new num[] { i , j };
+		}
+	   }
+	}
+	throw new IllegalArgumentException("No two sum solution");
+    }
 
     public static void main(String[] args) {
         MessedArrays m = new MessedArrays();
