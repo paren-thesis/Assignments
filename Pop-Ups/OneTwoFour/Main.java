7f45292d41e6610b6899b9c23891a889598009da
p@ren <ntimprosper308@gmail.com>
2024-11-11 20:49:00 +0000

    Enable random and comment methods to set values

--- Pop-Ups/OneTwoFour/Main.java
+++ Pop-Ups/OneTwoFour/Main.java
@@ -7,10 +7,10 @@ public class Main {
         System.out.println("Voice Time: " + rgVal.rVoice());
         
         // Optional: Display all default values without random generation
-        rgVal.setGenerateRandom(false); // Disable random generation
-        rgVal.setAirtime(20.00);
-        rgVal.setSMSunits(50.00);
-        rgVal.setVoice(120.00);
+        rgVal.setGenerateRandom(true); // Disable random generation
+        //rgVal.setAirtime(20.00);
+        //rgVal.setSMSunits(50.00);
+        //rgVal.setVoice(120.00);
 
         System.out.println("\n[Non-Random Values]");
         System.out.println("Airtime: " + rgVal.rAirtime());
